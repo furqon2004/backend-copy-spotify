1522ff544f6c6d30558df943851574c8b6dce631
dickyrahmat <dickyrahmatfurqon1234@gmail.com>
2026-03-03 11:33:09 +0800

    fix: add CORS preflight handling in .htaccess and HandleCors middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->api(prepend: [
+            \Illuminate\Http\Middleware\HandleCors::class,
             ForceJsonResponse::class,
         ]);
         $middleware->alias([
