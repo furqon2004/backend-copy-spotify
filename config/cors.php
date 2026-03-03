224f7dbfa1c775da03fd63ee4150ac785f87dfae
DanDev_ <danuart21@gmail.com>
2026-03-03 11:25:27 +0800

    fix: cors

--- config/cors.php
+++ config/cors.php
@@ -6,15 +6,15 @@ return [
 
     'allowed_methods' => ['*'],
 
-    // 'allowed_origins' => [
-    //     'http://localhost:3000',
-    //     'http://localhost:5173',
-    //     'http://127.0.0.1:3000',
-    //     'http://127.0.0.1:5173',
-    //     'https://spotify-fe-clone.vercel.app',
-    // ],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'http://localhost:5173',
+        'http://127.0.0.1:3000',
+        'http://127.0.0.1:5173',
+        'https://spotify-fe-clone.vercel.app',
+    ],
 
-    'allowed_origins' => ['*'],
+    // 'allowed_origins' => ['*'],
 
     'allowed_headers' => ['*'],
 
