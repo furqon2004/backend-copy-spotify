4469f62eba08d83c1dfd16b3611c96aa0831415d
dickyrahmat <dickyrahmatfurqon1234@gmail.com>
2026-03-03 11:56:01 +0800

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -6,21 +6,21 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'
-        // 'http://localhost:3000',
-        // 'http://localhost:5173',
-        // 'http://127.0.0.1:3000',
-        // 'http://127.0.0.1:5173',
-        // 'https://spotify-fe-clone.vercel.app',
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'http://localhost:5173',
+        'http://127.0.0.1:3000',
+        'http://127.0.0.1:5173',
+        'https://spotify-fe-clone.vercel.app',
     ],
 
-    // 'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
-    'max_age' => 0,
+    'max_age' => 86400,
 
     'supports_credentials' => true,
 
